d180a39e645edee305ccdef76829a26816bb1cb2
Kim Seok Il <thirty950@naver.com>
2018-07-29 19:21:21 +0900

    removed unnecessary annotations

--- datasplit.c
+++ datasplit.c
@@ -55,8 +55,6 @@ void que_push(Que * que, char * string){
 char * que_pop(Que * que){
 	char* ptr;
 	pthread_mutex_lock(&mutex);
-//	printf("que_pop entered\n");
-//	printf("que_pop mutex locked\n");
 	if(que->front > que->tail){
 		pthread_mutex_unlock(&mutex);
 		return NULL;
@@ -65,7 +63,6 @@ char * que_pop(Que * que){
 
 	que->front++;
 	pthread_mutex_unlock(&mutex);
-//	printf("que_pop done\n");
 	return ptr;
 }
 
@@ -160,7 +157,6 @@ void* workerFunc(void * a){
 		createProcessesForWorker(0, parentToChild[RDEND] ,childToParent[WREND]);
 		error_handling("createProcessesForWorker errror! child process not end.");
 	}
-//	waitpid(pid, &temp, 0);
 	close(parentToChild[RDEND]);
 	close(childToParent[WREND]);
 	fcntl(childToParent[RDEND], F_SETFL, O_NONBLOCK);
@@ -171,40 +167,29 @@ void* workerFunc(void * a){
 
 	while(1){
 		ptr = que_pop(&inputQue);
-		if(ptr != NULL){
+		if(ptr != NULL)
 			write(parentToChild[WREND], ptr, strlen(ptr));
-//			fsync(parentToChild[WREND]);
-//			printf("ptr = %s\n",ptr);
-		}
 		else if(ptr == NULL && run == 0){
 			int status;
 			if(inputQue.front <= inputQue.tail)
 				continue;
 			close(parentToChild[WREND]);
 			wait(&status);
-//			printf("%d %d wait done!\n", inputQue.front, inputQue.tail);
 			break;
 		}
 		if((readLen = read(childToParent[RDEND], staticBuf, MAX_SINGLE_INPUT)) > 0){
 			buf = (char*)calloc(1, sizeof(char) * MAX_SINGLE_INPUT);
 			strcpy(buf, staticBuf);
 			que_push(&outputQue, buf);
-
-//			printf("buf = %s\n",buf);
-//			fflush(stdout);
 		}
-
 	}
 
 	fcntl(childToParent[RDEND], F_SETFL, !O_NONBLOCK);
 	while(read(childToParent[RDEND], staticBuf, MAX_SINGLE_INPUT) > 0){
 		buf = (char*)calloc(1, sizeof(char) * MAX_SINGLE_INPUT);
 		strcpy(buf, staticBuf);
-//		printf("last : %s\n",buf);
 		que_push(&outputQue, buf);
 	}
-	
-//	printf("workerFunc Done!\n");
 	return NULL;
 }
 
@@ -225,9 +210,7 @@ void parseWorkerThreadProcessArgments(char * srcArgment){
 
 	threadArgcs[0] = 1;	
 	for(srcIdx = 0, i =0 ,j = 0, k = 0; ; srcIdx++){
-//		printf("%c\n",srcArgment[srcIdx]);
 		if(srcArgment[srcIdx] == '\0'){
-//			printf("end argv : %s",threadArgvs[i][0]);
 			threadArgvs[i][j][k] = 0;
 			threadArgvs[i][j + 1]= NULL;
 			break;
@@ -247,36 +230,23 @@ void parseWorkerThreadProcessArgments(char * srcArgment){
 		}else if(srcArgment[srcIdx] == ' ' && isSingleQuoted == 1){
 			threadArgvs[i][j][k++] = srcArgment[srcIdx];
 		}else if(srcArgment[srcIdx] == '\'' && isSingleQuoted == 0){
-//			threadArgvs[i][j][k++] = srcArgment[srcIdx];
 			isSingleQuoted = 1;
 		}else if(srcArgment[srcIdx] == '\'' && isSingleQuoted == 1){
-//			threadArgvs[i][j][k++] = srcArgment[srcIdx];
 			isSingleQuoted = 0;
 		}else{
 			threadArgvs[i][j][k++] = srcArgment[srcIdx];
 		}
 	}
-
-
-/*	for(i = 0; i <numOfThreadArgments; i++){
-		if(strcmp(threadArgvs[i][0], "grep") == 0){
-//			printf("let's strcpy i : %d threadArgcs[i] : %d\n", i, threadArgcs[i]);
-			threadArgvs[i][threadArgcs[i]] = (char*)calloc(1, sizeof(char)* GENERAL_INTEGER);
-			strcpy(threadArgvs[i][threadArgcs[i]++], "--line-buffered");
-			threadArgvs[i][threadArgcs[i]] = NULL;
-		}
-	}
-*/
 }
 
 void testFunction_showThreadArgments(){
-/*	int i, j;
+	int i, j;
 	for(i = 0;i < numOfThreadArgments;i++){
 		printf("%dth argc : %d\n",i,threadArgcs[i]);
 		for(j = 0; j<= threadArgcs[i]; j++){
 			printf("%d: %s\n",j,threadArgvs[i][j]);
 		}
-	}*/
+	}
 }
 
 void argmentErrorHandle(int argc, char * argv[]){
@@ -305,37 +275,24 @@ void mainLoop_pushToQue(char * string){
 	strcpy(pusher, ptr);
 	pusher[strlen(pusher)] = '\n';
 	pusher[strlen(pusher)] = 0;
-/*	sleep(1);
-	printf("pusher: %s ",ptr);
-	fflush(stdout);*/
-
-//	printf("%s",ptr);
-//	printf("asd\n");
 	que_push(&inputQue, pusher);
 
 	while((ptr = strtok(NULL, "\n"))){
 		pusher = (char*)calloc(1, sizeof(char) * (strlen(ptr) + 1));
 		strcpy(pusher, ptr);
-//		printf("pusher: %s",pusher);
 		pusher[strlen(pusher)] = '\n';
 		pusher[strlen(pusher)] = 0;
 		que_push(&inputQue, pusher);
-		//printf("%s",ptr);
 	}
 }
 
 void mainLoop(){
 	char* buf;
-//	int readSize;
 
 	buf = (char*)calloc(1,sizeof(char) * MAX_SINGLE_INPUT);
 
 	while((read(STDIN_FILENO, buf, MAX_SINGLE_INPUT)) > 0){
-//		printf("%s",buf);
-//		printf("main read Something\n");
 		mainLoop_pushToQue(buf);
-//		que_push(&inputQue, buf);
-		//alarm(0);
 		buf = (char*)calloc(1,sizeof(char) * MAX_SINGLE_INPUT);
 	}
 }
@@ -345,7 +302,6 @@ void waitUntilAllThreadsAreEnd(pthread_t receiver, pthread_t workers[]){
 	int wi, ti;
 	
 	run  = 0;
-//	signal(sigterm);
 
 	for(wi=0, ti =0;wi<numOfWorkerThreads;wi++){
 		ti = wi;
@@ -366,7 +322,7 @@ int main(int argc, char* argv[]){
 	que_init(&outputQue);
 	que_init(&inputQue);
 	parseWorkerThreadProcessArgments(argv[argc -1]);
-	testFunction_showThreadArgments();
+	//testFunction_showThreadArgments();
 	argmentErrorHandle(argc, argv);
 	notifyNumOfWorkerThreads(argv);
 
